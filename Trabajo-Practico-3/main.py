9490800baa2c9a37d6428d48102cff5c635f18be
Alejo <alejooodaniel45@gmail.com>
2025-10-03 16:57:25 -0300

    subida de archivos tp3

---
+++ Trabajo-Practico-3/main.py
@@ -0,0 +1,9 @@
+# =========================================
+# Trabajo Práctico n°3 - Programación 2
+# Sección B - Ejercicios 1 al 10
+# Integrantes:
+# - Alejo Paniagua
+# - Sabrina Bidal
+# - Francisco Diaz
+# =========================================
+

06e006280ceadcef2f7058cc3f392a978e904609
Alejo <alejooodaniel45@gmail.com>
2025-11-03 21:17:38 -0300

    Agregado de ejercicios 2 y 3

--- Trabajo-Final-Integrador/cliente.py
+++ Trabajo-Final-Integrador/cliente.py
@@ -1,3 +1,38 @@
-class Cliente:
+# =========================================
+# Ejercicio 2 - generar la clase cliente.py
+# =========================================
 
-    pass
+
+class Cliente:
+    # Constructor
+    def __init__(self, numeroId: int, nombres: str, apellidos: str, mail: str):
+        self.__numeroId = numeroId
+        self.__nombres = nombres
+        self.__apellidos = apellidos
+        self.__email = mail
+
+    # --- Comandos (modificadores) ---
+    def establecerNumeroId(self, numeroId: int):
+        self.__numeroId = numeroId
+
+    def establecerNombres(self, nombres: str):
+        self.__nombres = nombres
+
+    def establecerApellidos(self, apellidos: str):
+        self.__apellidos = apellidos
+
+    def establecerEmail(self, email: str):
+        self.__email = email
+
+    # --- Consultas (getters) ---
+    def obtenerNumeroId(self) -> int:
+        return self.__numeroId
+
+    def obtenerNombres(self) -> str:
+        return self.__nombres
+
+    def obtenerApellidos(self) -> str:
+        return self.__apellidos
+
+    def obtenerEmail(self) -> str:
+        return self.__email
--- Trabajo-Final-Integrador/sucursal.py
+++ Trabajo-Final-Integrador/sucursal.py
@@ -1,3 +1,35 @@
-class Sucursal:
+# =========================================
+# Ejercicio 1 - generar la clase sucursal.py
+# =========================================
 
-    pass
+
+class Sucursal:
+    # Constructor
+    def __init__(self, numeroId: int, direccion: str):
+        self.__numeroId = numeroId
+        self.__direccion = direccion
+        self.__ventas: List[Venta] = []
+
+    # --- Comandos (modificadores) ---
+    def establecerNumeroId(self, numeroId: int):
+        self.__numeroId = numeroId
+
+    def establecerDireccion(self, direccion: str):
+        self.__direccion = direccion
+
+    def agregarVenta(self, venta: Venta):
+        self.__ventas.append(venta)
+
+    def removerVenta(self, venta: Venta):
+        if venta in self.__ventas:
+            self.__ventas.remove(venta)
+
+    # --- Consultas ( ---
+    def obtenerNumeroId(self) -> int:
+        return self.__numeroId
+
+    def obtenerDireccion(self) -> str:
+        return self.__direccion
+
+    def obtenerVentas(self) -> List[Venta]:
+        return self.__ventas
